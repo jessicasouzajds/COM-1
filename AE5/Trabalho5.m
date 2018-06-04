9fb46f2b93ca8a7897dfec61f8898a6af18c5420
jessicasouzajds <jessica.souzajds@gmail.com>
2018-06-04 08:22:51 -0700

    Versão final do código referente à AE5

--- AE5/Trabalho5.m
+++ AE5/Trabalho5.m
@@ -6,40 +6,115 @@
 % O arquivo captura_jessicasouza.wav foi gerado a partir do codigo
 % desenvolvido em sala de aula: aula24.m
 
-[y,Fs] = audioread('captura_jessicasouza.wav');
-%extraindo as informaçoes do audio
-info_audio = audioinfo('captura_jessicahahn.wav');
-t = 0:seconds(1/Fs):seconds(info_audio.Duration);
-t = t(1:end-1);
+close all;
+clear all;
+clc;
 
-%ouvindo
-sound(y,Fs)
-figure();
-subplot(211);
-plot(t,y);
+% Extraindo caracteristicas do sinal
+[y,Fs] = audioread('captura_jessicasouza2.wav');
+info_audio = audioinfo('captura_jessicasouza2.wav');
+t = 0:1/Fs:5-1/Fs;
+k = 8;       % Bits de quantizacao: variar entre 3, 5, 8 e 13
+L = 2^k;     % Niveis de quantizacao
 
+% Quantizacao
+y_up_pos = y+1;                 % Inserindo offset de 1v no audio
+passo_delta = max(y_up_pos)/L;  % Volts por nivel
+y_q = y_up_pos/passo_delta;     % Sinal quantizado
+
+% Codificacao
+y_dec = round(y_q);             % Transformando para decimal
+y_bin = de2bi(y_dec);           % Transformando em binario para transmitir
+[m,n] = size(y_bin);
+y_dig = reshape(y_bin, 1, m*n); % Sinal em formato digital
+
+% min(y_q)
+% max(y_q)
+
+% min(y_dec)
+% max(y_dec)
 
 %%
-% Resumo do que é para fazer:
+% Transmitindo em um canal agwn uzando NRZ polar e filtro casado
+N = 10;
+y_dig2 = (y_dig*2)-1;
 
-% fs = 44100 (placa de som)
-% apos a placa de som, vai p matlab
-% escala de audio de -1 ate 1V
-% voz, vetor de 1 ate 220500 (5*fs)
-% plot(voz,t)
-% depois faz a quantizacao
-% fazer utilizando 3, 5 e 8 bits para k  (deixar generico para alterar estes valores na entrada)
-% 
-% quantizar para 3 niveis = L=2^3=8 niveis
-% primeiro desloca o sinal de 0 ate 2 -> voz=voz+1
-% calcular o passo delta = Amax/Niveis = 2/8 = 0.25V por nivel
-% voz = voz/delta
-% arredonda os valores para inteiro utilizando o round
-% round(voz)
-% depois vai para a codificacao
-% utiliza a funcao de2bi(voz) e o sinal esta pronto para transmitir e fazer o reshape
-% 
-% utiliza NRZ com fator N=10
-% transmite AWGN, recebe e faz o processo de conversao D/A
-% mantem a SNR constante entre 0,5 e 10 (e 100 limpa) (generico) e usa apenas 8 niveis de quantizacao
-% ouvir e diferenciar os efeitos da variazao de SNR
+% Gerando o filtro
+filtro_tx = ones(1,N);              % Pulso de N amostras
+filtro_rx = filtro_tx;
+info_up = upsample(y_dig2,N);       % Insere N amostras a cada valor da informacao
+
+% Passando o sinal pelo filtro
+y_tx = filter(filtro_tx,1,info_up); % Este é o sinal transmitido
+t2 = [0:5/length(y_tx):5-1/length(y_tx)];
+
+%%
+% Recepcao do sinal - Canal AWGN
+SNR = 100; % Variar SNR entre: 0, 5 e 10 para 8 niveis (fixo). Para variar os niveis mantem SNR=100 
+y_rx = awgn(y_tx,SNR);
+y_rx_filt = filter(filtro_rx,1,y_rx)/N;
+Z_t = y_rx_filt(N:N:end); % Pega o valor de bit no fim de cada pulso
+y_fim = (Z_t > 0);
+
+%%
+% Decodificação e restauração do sinal recebido
+y_dig_rx = reshape(y_fim, m, n);
+y_int = bi2de(y_dig_rx);
+
+y_int = y_int/L;      % Volta ao formato original
+y_int = y_int - 1;    % Remove o offset
+sound(y_int,Fs)
+%%
+% Plotando os resultados
+
+
+% Sinal pronto para transmitir
+figure,
+subplot(411),plot(t,y); % Sinal Original
+%ylim([-3.5 3.5]);
+title('(a)');
+xlabel('Tempo (s)');
+ylabel('Amplitude(v)');
+
+subplot(412),plot(t,y_q); % Sinal Quantizado
+%ylim([-3.5 3.5]);
+title('(b)');
+xlabel('Tempo (s)');
+ylabel('Amplitude(v)');
+
+subplot(413),plot(t,y_dec); % Sinal Codificado Decimal
+%ylim([-3.5 3.5]);
+title('(c)');
+xlabel('Tempo (s)');
+ylabel('Amplitude(v)');
+
+subplot(414),plot(t,y_int); % Sinal decodificado
+ylim([-1.2 1.2]);
+title('(d)');
+xlabel('Tempo (s)');
+ylabel('Amplitude(v)');
+
+
+
+% Sinal NRZ transmitido e recebido
+figure,
+subplot(311),plot(t2,y_tx); % sinal TX
+xlim([1.595 1.60]);
+ylim([-1.2 1.2]);
+title('(a)');
+xlabel('Tempo (s)');
+ylabel('Amplitude(v)');
+
+subplot(312),plot(t2,y_rx); % sinal RX
+xlim([1.595 1.60]);
+ylim([-2 2]);
+title('(b)');
+xlabel('Tempo (s)');
+ylabel('Amplitude(v)');
+
+subplot(313),plot(t2,y_rx_filt); % sinal RX filtrado
+xlim([1.595 1.60]);
+ylim([-1.2 1.2]);
+title('(c)');
+xlabel('Tempo (s)');
+ylabel('Amplitude(v)');
